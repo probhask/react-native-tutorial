743720777bb9669645d87699191741ee61e830fb
probhask <bhaskarneeraj23@gmail.com>
2025-12-07 19:06:55 +0530

    chore: update app configuration and dependencies
    - Integrate RootNavigator in App.tsx with ThemeProvider
    - Add navigation dependencies (@react-navigation packages)
    - Add gesture and animation libraries (reanimated, gesture-handler)
    - Configure babel for react-native-worklets support
    - Update React to 19.1.1 and React Native to 0.82.1

--- App.tsx
+++ App.tsx
@@ -1,45 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import RootNavigator from './src/RootNavigator';
+import { ThemeProvider } from './src/context/ThemeContext';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <ThemeProvider>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
